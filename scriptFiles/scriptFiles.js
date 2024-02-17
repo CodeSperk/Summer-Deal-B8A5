70ffc2d031b53531792a47ca6d30b7613b6b09b6
Mahbubur Rahman <inbx.mahbub@gmail.com>
2024-02-17 13:52:29 +0600

    applied coupon and discount

--- scriptFiles/scriptFiles.js
+++ scriptFiles/scriptFiles.js
@@ -2,44 +2,65 @@ let initialNumber = 1;
 let totalPrice = 0;
 let discount = 0;
 
-const cards = document.querySelectorAll('.card');
-for(let card of cards){
-  function handleCart(){
-    const title = card.querySelector('h2').innerText;
-    const priceValue = card.querySelector('p').innerText.split(' ')[0];
-    
+const cards = document.querySelectorAll(".card");
+for (let card of cards) {
+  function handleCart() {
+    const title = card.querySelector("h2").innerText;
+    const priceValue = card.querySelector("p").innerText.split(" ")[0];
+
     // added items in the cart (inside the ul)
     /* ------------------------
      const container = document.getElementById('item-container');
     const newLi = document.createElement('li');
     newLi.innerText = title;
     container.appendChild(newLi); */
-    
+
     //alternative way to give the number before added items (inside the div)
-    const container = document.getElementById('item-container');
-    const newP = document.createElement('p');
-    newP.innerText = initialNumber + ' ' + title;
+    const container = document.getElementById("item-container");
+    const newP = document.createElement("p");
+    newP.innerText = initialNumber + " " + title;
     container.appendChild(newP);
     initialNumber++;
 
     //total price in the cart
     totalPrice = totalPrice + parseFloat(priceValue);
-    const totalPriceContainer = document.getElementById('total-price-container');
+    const totalPriceContainer = document.getElementById(
+      "total-price-container"
+    );
     totalPriceContainer.innerText = totalPrice.toFixed(2);
-    
+
     //enable coupon & make purchase button
-    if(totalPrice>0){
-      document.getElementById('purchase-btn').removeAttribute('disabled');
+    if (totalPrice > 0) {
+      document.getElementById("purchase-btn").removeAttribute("disabled");
     }
-    if(totalPrice>=200){
-      document.getElementById('apply-btn').removeAttribute('disabled')
-    }
+    if (totalPrice >= 200) {
+      const inputEl = document.getElementById("coupon-input-field");
+      inputEl.removeAttribute("disabled");
+      inputEl.classList.add("border-[#E527B2]");
+      const applyBtn = document.getElementById("apply-btn");
+      applyBtn.removeAttribute("disabled");
 
-    // grand Total
-    const grandTotalContainer = document.getElementById('grand-total');
+      applyBtn.addEventListener("click", function () {
+        const inputValue = inputEl.value;
+        if (inputValue === "SELL200") {
+          discount = totalPrice * 0.2;
+
+
+          //set discount
+          document.getElementById('discount').innerText = discount.toFixed(2);
+
+          // grand Total
+          const grandTotalContainer = document.getElementById("grand-total");
+          grandTotalContainer.innerText = (totalPrice - discount).toFixed(2);
+
+          alert("You got 20% discount.");
+        } else {
+          alert("Please, enter valid coupon.");
+        }
+      });
+    }
+    const grandTotalContainer = document.getElementById("grand-total");
     grandTotalContainer.innerText = (totalPrice - discount).toFixed(2);
-    
-
-
-  }card.addEventListener('click', handleCart);
+  }
+  card.addEventListener("click", handleCart);
 }
