c7905766485740b30ea1224c67312c6f2e251e87
Mahbubur Rahman <inbx.mahbub@gmail.com>
2024-02-17 12:12:00 +0600

    alternative way to add list items in the cart

--- scriptFiles/scriptFiles.js
+++ scriptFiles/scriptFiles.js
@@ -1,16 +1,24 @@
+let initialNumber = 1;
+
 const cards = document.querySelectorAll('.card');
 for(let card of cards){
   function handleCart(){
     const title = card.querySelector('h2').innerText;
     const priceValue = card.querySelector('p').innerText.split(' ')[0];
     
-    // added items in the cart
-    const container = document.getElementById('item-container');
+    // added items in the cart (inside the ul)
+    /* ------------------------
+     const container = document.getElementById('item-container');
     const newLi = document.createElement('li');
     newLi.innerText = title;
-    container.appendChild(newLi);
+    container.appendChild(newLi); */
     
-
+    //alternative way to give the number before added items (inside the div)
+    const container = document.getElementById('item-container');
+    const newP = document.createElement('p');
+    newP.innerText = initialNumber + ' ' + title;
+    container.appendChild(newP);
+    initialNumber++;
 
 
   }card.addEventListener('click', handleCart);
